8251192445623993e8b80602322fd2746266513b
Marc Schultz <lunij@users.noreply.github.com>
2023-05-05 16:28:49 +0200

    Clean up package manifest

--- Package.swift
+++ Package.swift
@@ -9,42 +9,37 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        .executable(
-            name: "asc",
-            targets: ["appstoreconnect-cli"]
-        )
+        .executable(name: "asc", targets: ["asc"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/johnsundell/files.git",
-            from: "4.1.1"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-argument-parser",
-            exact: "1.1.3"
-        ),
-        .package(
-            url: "https://github.com/AvdLee/appstoreconnect-swift-sdk.git",
-            from: "1.7.0"
-        ),
-        .package(
-            url: "https://github.com/jpsim/Yams.git",
-            from: "5.0.1"
-        ),
-        .package(
-            url: "https://github.com/scottrhoyt/SwiftyTextTable.git",
-            from: "0.9.0"
-        ),
-        .package(
-            url: "https://github.com/dehesa/CodableCSV.git",
-            from: "0.5.5"
-        )
+        .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.1.3"),
+        .package(url: "https://github.com/AvdLee/appstoreconnect-swift-sdk.git", from: "1.7.0"),
+        .package(url: "https://github.com/dehesa/CodableCSV.git", from: "0.5.5"),
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.1.1"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
+        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(name: "appstoreconnect-cli", dependencies: ["AppStoreConnectCLI"]),
-        .target(name: "Model"),
+        .executableTarget(name: "asc", dependencies: ["AppStoreConnectCLI"]),
+        .target(
+            name: "AppStoreConnectCLI",
+            dependencies: [
+                .product(name: "AppStoreConnect-Swift-SDK", package: "AppStoreConnect-Swift-SDK"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "CodableCSV", package: "CodableCSV"),
+                .product(name: "SwiftyTextTable", package: "SwiftyTextTable"),
+                .product(name: "Yams", package: "Yams"),
+                .target(name: "Model"),
+                .target(name: "FileSystem")
+            ]
+        ),
+        .testTarget(
+            name: "AppStoreConnectCLITests",
+            dependencies: ["AppStoreConnectCLI"],
+            resources: [
+                .copy("Models/Fixtures.bundle")
+            ]
+        ),
         .target(
             name: "FileSystem",
             dependencies: [
@@ -53,24 +48,6 @@ let package = Package(
                 .product(name: "Files", package: "Files"),
             ]
         ),
-        .target(
-            name: "AppStoreConnectCLI",
-            dependencies: [
-                .target(name: "Model"),
-                .target(name: "FileSystem"),
-                .product(name: "AppStoreConnect-Swift-SDK", package: "AppStoreConnect-Swift-SDK"),
-                .product(name: "Yams", package: "Yams"),
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "SwiftyTextTable", package: "SwiftyTextTable"),
-                .product(name: "CodableCSV", package: "CodableCSV")
-            ]
-        ),
-        .testTarget(
-            name: "appstoreconnect-cliTests",
-            dependencies: ["appstoreconnect-cli"],
-            resources: [
-                .copy("Models/Fixtures.bundle")
-            ]
-        ),
+        .target(name: "Model")
     ]
 )
