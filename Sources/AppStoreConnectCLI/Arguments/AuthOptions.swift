ced6afcaa5959213b920f4a937540344e1066e13
Marc Schultz <lunij@users.noreply.github.com>
2023-05-15 17:47:02 +0200

    Update handling of credentials

--- Sources/AppStoreConnectCLI/Arguments/AuthOptions.swift
+++ Sources/AppStoreConnectCLI/Arguments/AuthOptions.swift
@@ -4,9 +4,20 @@ import ArgumentParser
 import Foundation
 
 struct AuthOptions: ParsableArguments {
-    enum Error: Swift.Error {
-        case issuerNotProvided
+    enum Error: Swift.Error, CustomStringConvertible, Equatable {
+        case issuerIdNotProvided
         case apiKeyIdNotProvided
+
+        var description: String {
+            switch self {
+            case .issuerIdNotProvided:
+                return "App Store Connect API Issuer ID not provided. "
+                    + "Either add it as option on command line or use the environment variable \(Environment.appStoreConnectIssuerId.rawValue)."
+            case .apiKeyIdNotProvided:
+                return "App Store Connect API Key ID not provided. "
+                    + "Either add it as option on command line or use the environment variable \(Environment.appStoreConnectApiKeyId.rawValue)."
+            }
+        }
     }
 
     @Option(
@@ -16,12 +27,12 @@ struct AuthOptions: ParsableArguments {
             """
             The value for this option can be obtained from the AppStore Connect portal and takes the form of a UUID.
 
-            If not specified on the command line this value will be read from the environment variable APPSTORE_CONNECT_ISSUER_ID.
+            If not specified on the command line this value will be read from the environment variable \(Environment.appStoreConnectIssuerId.rawValue).
             """,
             valueName: "uuid"
         )
     )
-    var apiIssuer: IssuerID = .environment("APPSTORE_CONNECT_ISSUER_ID")
+    var apiIssuer: String = Environment.appStoreConnectIssuerId.load()
 
     // swiftlint:disable line_length
     @Option(
@@ -31,14 +42,14 @@ struct AuthOptions: ParsableArguments {
             """
             The value for this option can be obtained from the AppStore Connect portal and takes the form of an 10 character alpha-numeric identifier, eg. 7MM5YSX5LS
 
-            If not specified on the command line the value of this option will be read from the environment variable APPSTORE_CONNECT_API_KEY_ID.
+            If not specified on the command line the value of this option will be read from the environment variable \(Environment.appStoreConnectApiKeyId.rawValue).
 
-            The environment will be searched for key with the name of \(APIKeyID.apiKeyEnvironmentKey). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
+            The environment will be searched for key with the name of \(Environment.appStoreConnectApiKey.rawValue). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
 
-            If the \(APIKeyID.apiKeyEnvironmentKey) environment variable is empty, in the incorrect format, or not found then the following directories will be searched in sequence for a private key file with the name of 'AuthKey_<keyid>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
+            If the \(Environment.appStoreConnectApiKey.rawValue) environment variable is empty, in the incorrect format, or not found then the following directories will be searched in sequence for a private key file with the name of 'AuthKey_<keyid>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
             """,
             valueName: "keyid"
         )
     )
-    var apiKeyId: APIKeyID = .environment("APPSTORE_CONNECT_API_KEY_ID")
+    var apiKeyId: APIKeyID = Environment.appStoreConnectApiKeyId.load()
 }
--- Sources/AppStoreConnectCLI/Helpers/APIConfiguration+Helpers.swift
+++ Sources/AppStoreConnectCLI/Helpers/APIConfiguration+Helpers.swift
@@ -6,18 +6,26 @@ import Yams
 
 extension APIConfiguration {
     init(_ authOptions: AuthOptions) throws {
-        guard authOptions.apiIssuer.value.isEmpty == false else {
-            throw AuthOptions.Error.issuerNotProvided
+        guard authOptions.apiIssuer.isEmpty == false else {
+            throw AuthOptions.Error.issuerIdNotProvided
         }
 
-        guard authOptions.apiKeyId.value.isEmpty == false else {
+        guard authOptions.apiKeyId.isEmpty == false else {
             throw AuthOptions.Error.apiKeyIdNotProvided
         }
 
         self.init(
-            issuerID: authOptions.apiIssuer.value,
-            privateKeyID: authOptions.apiKeyId.value,
-            privateKey: try authOptions.apiKeyId.load()
+            issuerID: authOptions.apiIssuer,
+            privateKeyID: authOptions.apiKeyId,
+            privateKey: try authOptions.apiKeyId.loadKey().extractPrivateKeyValue()
         )
+    }
+}
+
+private extension String {
+    func extractPrivateKeyValue() -> String {
+        components(separatedBy: .newlines)
+            .filter { !$0.hasSuffix("PRIVATE KEY-----") }
+            .joined()
     }
 }
